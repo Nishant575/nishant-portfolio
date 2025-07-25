a1da7768b24e97c3f993264c149fd7f6768f50fa
Nishant575 <n.dalvi2010@gmail.com>
2025-07-25 01:27:10 -0500

    errors solved

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -6,7 +6,6 @@ import MDXContent from '@/components/mdx-content'
 import { getPosts, getPostBySlug } from '@/lib/posts'
 import { ArrowLeftIcon } from '@radix-ui/react-icons'
 import { notFound } from 'next/navigation'
-import NewsletterForm from '@/components/newsletter-form'
 
 export async function generateStaticParams() {
   const posts = await getPosts()
@@ -59,9 +58,6 @@ export default async function Post({ params }: { params: { slug: string } }) {
           <MDXContent source={content} />
         </main>
 
-        <footer className='mt-16'>
-          <NewsletterForm />
-        </footer>
       </div>
     </section>
   )
